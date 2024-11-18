b60746bb9d1e06d7a44615cb56b006049fd0a0f7
MURAGEH <muragehdeveloper@gmail.com>
2024-11-18 15:33:54 +0300

    make sidebar collapsible - also, handle group menus in sidebar
    Signed-off-by: MURAGEH <muragehdeveloper@gmail.com>

--- src/layouts/MainSidebar.tsx
+++ src/layouts/MainSidebar.tsx
@@ -1,13 +1,29 @@
 import {FaBars, FaHouse, FaRegRectangleList, FaUser, FaUserTie} from "react-icons/fa6";
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import Accordion from "../components/base/Accrodion.tsx";
+
+interface NormalMenuItem {
+    icon: React.ReactNode;
+    label: string;
+    link: string;
+    group?: false;
+}
+
+interface GroupMenuItem {
+    group: true;
+    label: string;
+    children: NormalMenuItem[];
+}
+
+type MenuItem = NormalMenuItem | GroupMenuItem;
 
 const MainSidebar = () => {
     const [selected, setSelected] = React.useState<string | null>("/dashboard");
     const [isExpanded, setIsExpanded] = React.useState<boolean>(true);
     const navigate = useNavigate();
 
-    const MenuItems = [
+    const MenuItems: MenuItem[] = [
         {
             icon: <FaHouse/>,
             label: "Dashboard",
@@ -17,22 +33,26 @@ const MainSidebar = () => {
             icon: <FaRegRectangleList/>,
             label: "Seminars",
             link: "/dashboard/seminars"
-        }
-        ,
+        },
         {
-            icon: <FaUserTie/>,
-            label: "Employees",
-            link: "/dashboard/employees"
-        }
-        ,
-        {
-            icon: <FaUser/>,
-            label: "Customers",
-            link: "/dashboard/customers"
+            group: true,
+            label: "Extra BC functionality",
+            children: [
+                {
+                    icon: <FaUserTie/>,
+                    label: "Employees",
+                    link: "/dashboard/employees",
+                },
+                {
+                    icon: <FaUser/>,
+                    label: "Customers",
+                    link: "/dashboard/customers",
+                }
+            ]
         }
     ];
 
-    const Button = ({children, link, label}: { children: React.ReactNode, link: string, label: string }) => {
+    const MenuButton = ({children, link, label}: { children: React.ReactNode, link: string, label: string }) => {
         return (
             <button
                 onClick={() => {
@@ -40,7 +60,7 @@ const MainSidebar = () => {
                     return setSelected(link);
                 }}
                 title={label}
-                className={`min-w-12 h-12 flex items-center justify-${isExpanded ? 'start' : 'center'} gap-4 rounded-md text-${selected === link ? 'primary' : 'slate-900'} bg-transparent hover:bg-blue-700`}>
+                className={`w-full px-2 h-12 flex items-center justify-${isExpanded ? 'start' : 'center'} gap-4 rounded-md text-${selected === link ? 'primary' : 'slate-900'} bg-transparent hover:bg-blue-700`}>
                 {
                     React.cloneElement(children as React.ReactElement, {
                         size: 24,
@@ -54,16 +74,26 @@ const MainSidebar = () => {
 
     return (
         <div
-            className={`h-screen bg-white shadow-lg flex flex-col items-${isExpanded ? 'start' : 'center'} justify-start py-4 px-4 space-y-4 ${isExpanded ? 'min-w-48 max-w-64' : 'w-16'}`}>
+            className={`mainSidebar h-screen bg-white shadow-lg flex flex-col items-${isExpanded ? 'start' : 'center'} justify-start py-4 px-4 space-y-4 ${isExpanded ? 'min-w-48 max-w-72' : 'w-16'} transition-all duration-500 ease-in-out`}>
             <button
                 onClick={() => setIsExpanded(!isExpanded)}
-                className={`w-12 h-12 flex items-center justify-${isExpanded ? 'start' : 'center'} rounded-md bg-transparent hover:bg-blue-700`}>
+                className={`w-12 h-12 flex items-center justify-${isExpanded ? 'center' : 'center'} rounded-md bg-transparent hover:bg-blue-700`}>
                 <FaBars size={24}/>
             </button>
             {MenuItems.map((item, index) => (
-                <Button key={index} link={item.link} label={item.label}>
-                    {item.icon}
-                </Button>
+                item.group ? (
+                    <Accordion key={index} title={item.label} theme="plain" collapsed={!isExpanded}>
+                        {item.children.map((child, childIndex) => (
+                            <MenuButton key={childIndex} link={child.link} label={child.label}>
+                                {child.icon}
+                            </MenuButton>
+                        ))}
+                    </Accordion>
+                ) : (
+                    <MenuButton key={index} link={item.link} label={item.label}>
+                        {item.icon}
+                    </MenuButton>
+                )
             ))}
         </div>
     );
